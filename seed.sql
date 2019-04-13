044e91b794344d13a5b637ab661e0ee63f026a5c
CodeXRook <lacekitfly@gmail.com>
2019-04-13 17:12:23 -0400

    looking to connect db

--- seed.sql
+++ seed.sql
@@ -6,35 +6,38 @@ DROP DATABASE IF EXISTS movie_app;
  CREATE TABLE movies (
      id SERIAL PRIMARY KEY,
      stars INT NULL,
-     genre_id INT UNIQUE NOT NULL,
+     genres_id INT UNIQUE NOT NULL,
      img_url VARCHAR NOT NULL
  );
 
  CREATE TABLE genres(
-     id INT REFERENCES genre(id) NOT NULL,
+     id INT REFERENCES genres(id) NOT NULL,
      name VARCHAR NOT NULL
  );
 
  CREATE TABLE ratings (
      id SERIAL PRIMARY KEY,
      stars INT NULL,
-     movie_ INT REFERENCES movies(id)
+     movies_ INT REFERENCES movies(id)
  );
 
  CREATE TABLE comments(
      id SERIAL PRIMARY KEY,
      text VARCHAR NOT NULL,
-     movie_id INT REFERENCES movies(id) NOT NULL
+     movies_id INT REFERENCES movies(id) NOT NULL
  );
 
-INSERT INTO movies (id, title, genre_id) VALUES
-(' ', ' ', ' ');
+INSERT INTO movies (id, title, genres_id) VALUES
+(34, 'The Matrix', 'Action');
 
 INSERT INTO genres ( id, name) VALUES
-(' ',' ');
+(12,'Action');
 
-INSERT INTO ratings ( id, stars, movie_id) VALUES
-(' ', ' ', ' ');
+INSERT INTO ratings ( id, stars, movies_id) VALUES
+( 2, 5, 34);
+
+INSERT INTO comments( id, text, movies_id) VALUES
+( 5,'BEST MOVIE EVER', 34);
 
 
 
