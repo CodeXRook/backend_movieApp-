adcf53f3ceeabb3afb4d59ffae7f76df22b0369b
CodeXRook <lacekitfly@gmail.com>
2019-04-14 01:44:30 -0400

    updated the right index

--- src/index.js
+++ src/index.js
@@ -1,14 +1,1 @@
-const express = require('express');
-const router = express();
-
-
-router.get('/', (req, res, next) =>{
-    res.json('What is this');
-});
-
-router.use('/movies', require('./movies'));
-router.use('/ratings', require('./ratings'));
-router.use('/genres', require('./genres'));
-router.use('/comments', require('./comments'));
-
-module.eports = router;
+ 
---
+++ src/routes/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router = express();
+
+
+router.get('/', (req, res, next) =>{
+    res.json('What is this');
+});
+
+router.use('/movies', require('./movies'));
+router.use('/ratings', require('./ratings'));
+router.use('/genres', require('./genres'));
+router.use('/comments', require('./comments'));
+
+module.eports = router;
