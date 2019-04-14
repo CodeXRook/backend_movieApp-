8aeca536dce8815ddfd7a1d3bb3dd39b477bde3f
CodeXRook <lacekitfly@gmail.com>
2019-04-13 22:34:55 -0400

    updated genre

--- src/services/genres.js
+++ src/services/genres.js
@@ -1,2 +1,12 @@
 const { db } = require('./dbConnect');
 const GenreService = {};
+
+//CREATE
+GenreService.read = (id) => {
+    const sql = `
+    SELECT *
+    FROM genres
+    Where
+    genre.id $[id]
+    `;
+};
