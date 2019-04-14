535e5e0bc4848f5387efaa555bf1dcb30def6ca9
CodeXRook <lacekitfly@gmail.com>
2019-04-14 00:52:50 -0400

    updated get

--- src/routes/ratings.js
+++ src/routes/ratings.js
@@ -1,3 +1,8 @@
 const express = require('express');
 const ratingRouter = express.Router();
 const RatingService = require('../services/ratings');
+
+// Get rating info 
+ratingRouter.get('/:id', (req, res, next)=>{
+    
+})
