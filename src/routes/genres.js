207ce6bba9af3aed4b568131ea64d7d713a2555c
CodeXRook <lacekitfly@gmail.com>
2019-04-14 00:28:13 -0400

    added get

--- src/routes/genres.js
+++ src/routes/genres.js
@@ -1,3 +1,8 @@
 const express = require('express');
 const genreRouter = express.Router();
 const GenreService = require('../services/genres');
+
+// GET GENRE
+genreRouter.get('/:id', (req, res, nexdt) => {
+   
+})
