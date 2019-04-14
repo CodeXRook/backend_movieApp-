c07dfa45e9e8aa19e9e8175736fd8127a655a272
CodeXRook <lacekitfly@gmail.com>
2019-04-14 01:33:11 -0400

    updated then

--- src/routes/comments.js
+++ src/routes/comments.js
@@ -46,4 +46,9 @@ commentRouter.put('/:id', (req, res, next)=> {
 // Delete Comment
 commentRouter.delete('/:id', (req, res, next) => {
     const { id } = req.params;
-})
+
+    CommentService.delete(id)
+    .then(() => {
+    
+     })
+});
