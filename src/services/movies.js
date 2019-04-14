02ef814038a3964e721917b9dd4b591779fe6e98
CodeXRook <lacekitfly@gmail.com>
2019-04-13 22:19:10 -0400

    need to finish delete

--- src/services/movies.js
+++ src/services/movies.js
@@ -40,3 +40,9 @@ MovieService.update = (id, title, genre_id, img_url) => {
     return db.none(sql, {id, title, genre_id, img_url });
 };
 
+//DELETE
+MovieService.delete = (id) => {
+    const sql = `
+
+    `;
+};
